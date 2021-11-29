ed37bc70abbf2fa8519a44c4e754799e7413e8c6
aurelBichop <linux.aurelien@gmail.com>
2021-11-29 11:12:43 +0100

    exercices de base avec Vue.js

---
+++ src/components/Contenu.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container mt-5">
+        <liste v-bind:myArr="myArr"></liste>   
+    </div>
+</template>
+
+<script>
+import Liste from './Liste.vue'
+
+export default {
+    name: 'Contenu',
+    data: function(){
+        return {
+            myArr: [
+                {titre: 'Inception',date: 2010},
+                {titre: 'Avatar',date: 2009},
+                {titre: 'Seven',date: 1995},               
+            ]
+        }
+    },
+    components: {
+        'liste': Liste
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
---
+++ src/components/Liste.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+    <ul class="liste">
+        <li v-for="(film, index) in myArr" v-bind:key="index">
+           <div class="card">
+               <div class="card-body">
+                    Titre : {{ film.titre }}
+                    <br>
+                    Date : {{ film.date }}
+               </div>
+           </div>
+        </li>
+    </ul>
+    <div v-on:click="supprFilm" class="btn btn-danger mt-2">Supprime le film</div>
+</div>
+
+</template>
+
+
+<script>
+
+export default {
+    name: 'Liste',
+    data(){
+        return{
+            fruits: ['Orange','Papaye','Fraise']
+        }
+    },
+    methods: {
+        supprFilm: function(){
+            this.myArr.pop()
+        }
+    },
+    props: ['myArr']
+}
+
+</script>
+
+<style scoped src="./Liste/Liste.css"></style>
